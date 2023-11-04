234ee0b96275a9e315aaf1770d222310745651f8
MS <matthias.schramm2@gmail.com>
2023-11-04 17:14:07 +0100

    pipeline run via bash

--- pointcloud/pipeline.py
+++ pointcloud/pipeline.py
@@ -15,13 +15,12 @@ if os.path.exists(input_directory) and os.path.isdir(input_directory):
     # Print files
     print("Dateien im Verzeichnis '{}':".format(input_directory))
     print(files)
+    
     for file in files:
         print(f"Filtere {file}")
         basisname, dateiendung = os.path.splitext(file)
         output_filename = f"{basisname}_ground{dateiendung}"
-        print("----------------------outputfile")
-        print(output_filename)
-        
+
         pdal_pipeline = {
             "pipeline": [
                 f"{input_directory}/{file}",
@@ -40,26 +39,9 @@ if os.path.exists(input_directory) and os.path.isdir(input_directory):
             ]
         }
 
-        # PDAL Pipeline als JSON-String kodieren
         pipeline_json = json.dumps(pdal_pipeline)
-        print("----------------------JSON")
-        print (pipeline_json)
-
-        # PDAL-Befehl mit der Pipeline als JSON an PDAL übergeben
-        command = ["pdal", "pipeline", "--stdin", "-i", pipeline_json]
-        print("----------------------Command")
-        print(command)
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-
-        # Warte auf das Ergebnis und erhalte die Ausgabe
-        stdout, stderr = process.communicate()
-
-        # Ausgabe des Ergebnisses
-        print("Standard Output:")
-        print(stdout)
-
-        print("Standard Error:")
-        print(stderr)
+        command = f'pdal pipeline --stdin <<< \'{pipeline_json}\''
+        subprocess.run(command, shell=True, executable='../bin/bash')
                 
 else:
     print("Das Verzeichnis '{}' existiert nicht oder ist nicht zugänglich.".format(input_directory))
